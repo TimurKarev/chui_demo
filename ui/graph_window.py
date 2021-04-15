5a48cb430748170e093b14b99ede04c5bc6f3bcc
TimurKarev <timurkarev@mail.ru>
2021-04-15 16:57:42 +0300

    spider grath in progress

--- ui/graph_window.py
+++ ui/graph_window.py
@@ -3,24 +3,58 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5 import QtWidgets, Qt, QtCore
+
+from models.data_model import DataModel
 
 
 class Canvas(FigureCanvas):
     def __init__(self, parent):
-        fig, self._ax = plt.subplots(figsize=(5,4), dpi=200)
+        fig, self._ax = plt.subplots(figsize=(3, 2), dpi=200)
         super().__init__(fig)
         self.setParent(parent)
 
-        t = np.arange(0.0, 2.0, 0.01)
-        s = 1 + np.sin(2 * np.pi * t)
+        df = DataModel().df.iloc[:, 1].value_counts()
 
-        self._ax.plot(t, s)
-        self._ax.grid()
+        labels = df.index
+        sizes = list(df)
+
+        size_len = len(sizes)
+        explode = [0] * size_len
+        if size_len > 2:
+            e = sizes.index(max(sizes))
+            explode[int(e)] = 0.05
+
+        self._ax.pie(sizes,
+                     labels=labels,
+                     explode=explode,
+                     autopct='%1.1f%%',
+                     radius=1,
+                     wedgeprops=dict(width=0.6,
+                                     edgecolor='w',
+                                     ),
+                     )
 
 
-class GraphWindow(QMainWindow):
+class GraphWindow(QtWidgets.QMainWindow):
     """Main MainWindow."""
     def __init__(self, parent):
         super().__init__(parent=parent)
-        chart = Canvas(self)
+        try:
+            layout = QtWidgets.QVBoxLayout()
+
+            header1 = QtWidgets.QLabel()
+            header1.setAlignment(QtCore.Qt.AlignCenter)
+            header1.setText('<h1>Внешняя Внутреняя<h1>')
+            layout.addWidget(header1)
+
+            chart = Canvas(self)
+            layout.addWidget(chart)
+
+            layout.addStretch()
+
+            widget = QtWidgets.QWidget()
+            widget.setLayout(layout)
+            self.setCentralWidget(widget)
+        except Exception as e:
+            print(e)
